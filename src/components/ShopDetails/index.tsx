2bab50b4229887ba98d54d041f4458ebe9e2d27e
Chance Weston <chanceweston97@gmail.com>
2025-11-21 16:09:21 -0600

    feedback

--- src/components/ShopDetails/index.tsx
+++ src/components/ShopDetails/index.tsx
@@ -254,7 +254,7 @@ const ShopDetails = ({ product }: { product: Product }) => {
 
   return (
     <>
-      <section className="relative pt-10 pb-10 overflow-hidden lg:pt-15 xl:pt-15">
+      <section className="relative pt-10 pb-10 overflow-hidden lg:pt-[159px] xl:pt-[159px]">
         <div className="w-full px-4 mx-auto max-w-[1340px] sm:px-6 xl:px-0 ">
           <div className="flex flex-col lg:flex-row gap-7.5 xl:gap-16">
             {/* LEFT: GALLERY */}
@@ -471,20 +471,13 @@ const ShopDetails = ({ product }: { product: Product }) => {
         </div>
       </section>
       <div className="w-full">
-        <div className="w-full mx-auto px-4 sm:px-8 xl:px-0">
+        <div className="w-full mx-auto max-w-[1340px] px-4 sm:px-8 xl:px-0">
           <div className="grid grid-cols-1 sm:grid-cols-3 gap-1 mt-5">
             {productDetailsHeroData.map((item, index) => {
-              const desktopAlign =
-                index === 0
-                  ? "sm:justify-end"      // first item right
-                  : index === 1
-                    ? "sm:justify-center"   // second item center
-                    : "sm:justify-start";   // third item left
-
               return (
                 <div
                   key={index}
-                  className={`flex items-center gap-4 bg-[#F6F7F7] py-6 px-3 justify-center ${desktopAlign}`}
+                  className="flex items-center gap-4 bg-[#F6F7F7] py-6 px-3 justify-center"
                 >
                   <div className="flex items-center justify-center flex-shrink-0">
                     <Image
